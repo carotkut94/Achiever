cb0a5f88630fcd120650d8bfa26f2507010a6383
Death Code <deathcode@Deaths-MacBook-Pro.local>
2017-12-09 18:25:55 +0530

    Added UI Elements, and coredata model+craeted add goals UI

---
+++ Achiever/Enums/GoalType.swift
@@ -0,0 +1,9 @@
+//
+//  GoalType.swift
+//  Achiever
+//
+//  Created by Death Code on 09/12/17.
+//  Copyright © 2017 death code. All rights reserved.
+//
+
+import Foundation
---
+++ Achiever/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  Achiever
+//
+//  Created by Death Code on 09/12/17.
+//  Copyright © 2017 death code. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
